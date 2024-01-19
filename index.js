6f150cad5e089e0be0a07a2c05c0820c536cf1d3
Andrea Sattin <79341965+ilgrigio@users.noreply.github.com>
2024-01-19 12:34:03 +0100

    Inizio esercizio

---
+++ index.js
@@ -0,0 +1,12 @@
+// https://jsonplaceholder.typecode.com/users
+const getUsers = async () => {
+  try {
+    const apiRequest = await fetch(
+      "https://jsonplaceholder.typecode.com/users"
+    );
+    let resp = await queryUser.json();
+    console.log(resp);
+  } catch (err) {
+    console.error("Errore" + err);
+  }
+};
