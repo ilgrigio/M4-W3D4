12065e2580e95f99a59fb3e26d8b5922ad5fbd20
Andrea Sattin <79341965+ilgrigio@users.noreply.github.com>
2024-01-20 16:12:45 +0100

    value searchbar

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-let htmlUsers = [];
+let htmlUsers = "";
+let users = [];
 const getUsers = async () => {
   try {
     const apiRequest = await fetch(
       "https://jsonplaceholder.typicode.com/users"
     );
-    let users = await apiRequest.json();
+    users = await apiRequest.json();
     console.log(users);
+    users.map((usr) => {
+      console.log(usr);
+      htmlUsers += `
+      <tr>
+          <td id="${usr.id}">${usr.id}</td>
+          <td id="${usr.name}">${usr.name}</td>
+          <td id="${usr.username}">${usr.username}</td>
+          <td id="${usr.phone}">${usr.phone}</td>
+          <td id="${usr.email}">${usr.email}</td>
+      </tr>
+                  `;
+      document.getElementById("users").innerHTML = htmlUsers;
+    });
   } catch (err) {
     console.error("Errore" + err);
   }
-  users.map((urs) => {
-    htmlUsers += `<div class="row">
-            <div class="col">
-                <ol>
-                    <li id="${urs.id}"></li>
-                    <li id="${urs.name}"></li>
-                    <li id="${urs.username}"></li>
-                    <li id="${urs.phone}"></li>
-                    <li id="${urs.email}"></li>
-                </ol>
-            </div>
-        </div>`;
-    document.getElementById("users").innerHTML = htmlUsers;
-  });
+  // Created function to cicling searchbar and select value
+  const searchResult = () => {
+    const valueSearchBar = searchBar.value;
+    const valueSelect = selectOption.value;
 
-  let mainDiv = document.createElement("div");
-  mainDiv.innerText = "This is a paragraph";
-  document.body.appendChild(mainDiv);
+    let filterUsers = users.filter((user) => {
+      user[valueSelect]
+        .toLowerCase()
+        .indexOf(valueSearchBar.toLowerCase().trim() !== -1);
+
+      // user[valueBar].indexOf();
+      console.log(user[valueSelect]);
+    });
+  };
+  // Create variable to select field
+  const selectOption = document.getElementById("select_options");
+  const searchBar = document.getElementById("searchField");
+  const searchButton = document.getElementById("Search");
+  // Add event to search button
+  searchButton.addEventListener("click", searchResult);
 };
 getUsers();
